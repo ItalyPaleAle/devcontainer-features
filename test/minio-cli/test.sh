4f7574ce96e5b3876299a2088145b748e9575b9b
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2022-10-25 14:38:13 +0000

    Fixed tests

--- test/minio-cli/test.sh
+++ test/minio-cli/test.sh
@@ -17,17 +17,17 @@
 # 
 # This test can be run with the following command (from the root of this repo)
 #    devcontainer features test \ 
-#               --features color \
-#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu .
+#               --features minio-cli \
+#               --base-image mcr.microsoft.com/devcontainers/base:debian-11 .
 
 set -e
 
-# Optional: Import test library bundled with the devcontainer CLI
+# Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" color | grep 'my favorite color is red'
+check "version" mc --version | grep "mc version"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
